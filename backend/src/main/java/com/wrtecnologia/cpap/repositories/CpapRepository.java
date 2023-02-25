359467e1fcbffa2e56ad1c7fde50b6983c676bf2
wagnerdba <wagnerdba@gmail.com>
2023-02-24 21:59:15 -0300

    order by jpql

--- backend/src/main/java/com/wrtecnologia/cpap/repositories/CpapRepository.java
+++ backend/src/main/java/com/wrtecnologia/cpap/repositories/CpapRepository.java
@@ -21,13 +21,13 @@ public interface CpapRepository extends JpaRepository<Cpap, Long> {
     Page<Cpap> findByData(LocalDate min, LocalDate max, Pageable pageable);
 
     @Query("SELECT new com.wrtecnologia.cpap.dtos.CpapEventsDTO(obj.id, TO_CHAR(obj.data,'DD/MM/YYYY'), obj.eventos_hora) "
-            + " FROM Cpap AS obj ORDER BY obj.data")  //JPQL WHERE obj.id >= 37 // WHERE EXTRACT(MONTH FROM obj.data) = 2 //
+            + " FROM Cpap AS obj ORDER BY obj.data DESC")  //JPQL WHERE obj.id >= 37 // WHERE EXTRACT(MONTH FROM obj.data) = 2 //
     List<CpapEventsDTO> eventsByMonth();
 
     // TODO: CONSULTA A SER CONSTRUIDA (MEDIA DE EVENTOS POR MÊS)
 
     @Query("SELECT new com.wrtecnologia.cpap.dtos.CpapAverageEventsByMonthDTO(TO_CHAR(EXTRACT(MONTH FROM obj.data), 'fm00'), SUM(obj.eventos_hora) / COUNT(obj.id))"
-            + " FROM Cpap AS obj GROUP BY EXTRACT(MONTH FROM obj.data)")
+            + " FROM Cpap AS obj GROUP BY EXTRACT(MONTH FROM obj.data) ORDER BY EXTRACT(MONTH FROM obj.data)")
     List<CpapAverageEventsByMonthDTO> averageEventsByMonth();
 }
 
