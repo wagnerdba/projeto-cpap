824084c78f4cae0811c869af4d04b30252b64519
wagnerdba <wagnerdba@gmail.com>
2023-02-25 16:03:16 -0300

    schema created "cpap"

--- backend/src/test/java/com/wrtecnologia/cpap/CpapApplicationTests.java
+++ backend/src/test/java/com/wrtecnologia/cpap/CpapApplicationTests.java
@@ -1,9 +1,7 @@
 package com.wrtecnologia.cpap;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
 @SpringBootTest
 class CpapApplicationTests {
