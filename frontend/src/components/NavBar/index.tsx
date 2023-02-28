3e4bb3a4519e990ac260b05375d4d458cda9cd9d
wagnerdba <wagnerdba@gmail.com>
2023-02-28 16:40:45 -0300

    Update home page

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
-      <div className="container">
+      <div className="container text-center" >
         <nav className="my-2 my-md-0 mr-md-3">
           <Link to="/">
             <img src={ImgDsDark} alt="WR - Tecnologia em Sistemas" width="297" />
