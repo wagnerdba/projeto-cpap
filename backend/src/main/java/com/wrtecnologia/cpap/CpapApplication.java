5de27f08dd236e4b9aa9ae9e9b62754c21cebcbf
wagnerdba <wagnerdba@gmail.com>
2023-03-01 11:04:58 -0300

    Timezone local and Actuator adjusts

--- backend/src/main/java/com/wrtecnologia/cpap/CpapApplication.java
+++ backend/src/main/java/com/wrtecnologia/cpap/CpapApplication.java
@@ -1,11 +1,26 @@
 package com.wrtecnologia.cpap;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 @SpringBootApplication
+@Slf4j
 public class CpapApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CpapApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+
+		// Setting Spring Boot SetTimeZone
+		log.info(String.valueOf(ZoneId.systemDefault()));
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+		log.info(String.valueOf(ZoneId.systemDefault()));
 	}
 }
---
+++ backend/src/main/java/com/wrtecnologia/cpap/utils/AppHealth.java
@@ -0,0 +1,37 @@
+package com.wrtecnologia.cpap.utils;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class AppHealth implements HealthIndicator {
+
+    protected static final Map<String, Object> map = new HashMap<>();
+
+    @Override
+    public Health health(){
+        map.put("ActualDateTimeAppServer...: ", getDataHora());
+        return Health.up().withDetails(map).build();
+    }
+
+    @PostConstruct
+    public void init() {
+        map.put("StartedSpringApplicationIn: ", getDataHora());
+    }
+
+    public void run() {
+    }
+
+    private String getDataHora() {
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE dd/MM/uuuu HH:mm:ss");
+        return date.format(formatter);
+    }
+}
