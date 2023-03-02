64ac1b98ab7634f271cb4c212453d81869fa0bf0
wagnerdba <wagnerdba@gmail.com>
2023-03-02 15:04:19 -0300

    Footer adjusts

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,14 +1,16 @@
 const Footer = () => {
   return (
-    <footer>
-        <div className="text-center text-light align-items-center p-3 px-md-4 mb-3 border-bottom back-logo">
-            App desenvolvido por:{" "}<a href="https://github.com/wagnerpires" target="_blank" rel="noreferrer"> Wagner Pires</a>
-            <br />
-              <small>
-                <strong>WR - TECNOLOGIA EM SISTEMAS - (61)98162-6852 - wagnerdba@gmail.com</strong>
-              </small>
-        </div>
-    </footer>
+          <footer className="footer mt-auto bg-dark">
+          <div className="text-center text-light align-items-center p-3 px-md-4 mb-3 border-bottom back-logo" >
+            <div className="container ">
+              App desenvolvido por:{" "}<a href="https://github.com/wagnerpires" target="_blank" rel="noreferrer"> Wagner Pires</a>
+              <br />
+                <small>
+                  <strong>WR - TECNOLOGIA EM SISTEMAS - (61)98162-6852 - wagnerdba@gmail.com</strong>
+                </small>
+            </div>
+          </div>
+          </footer>
   );
 };
 
