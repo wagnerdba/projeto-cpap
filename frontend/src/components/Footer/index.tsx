5a5f86cadc2bad7f59516ecbce7bdd0f91de4390
wagnerdba <wagnerdba@gmail.com>
2023-03-02 15:12:44 -0300

    Update Footer (Border bottom)

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
           <footer className="footer mt-auto bg-dark">
-          <div className="text-center text-light align-items-center p-3 px-md-4 mb-3 border-bottom back-logo" >
+          <div className="text-center text-light align-items-center p-3 px-md-4 mb-3 back-logo" >
             <div className="container ">
               App desenvolvido por:{" "}<a href="https://github.com/wagnerpires" target="_blank" rel="noreferrer"> Wagner Pires</a>
               <br />
